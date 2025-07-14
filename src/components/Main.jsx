864e58e1283246be447670163a3c2ef243d4deec
rolindaz <rolinda.zamboni@gmail.com>
2025-07-14 14:23:28 +0200

    Enhance Main component layout with new sections for Digital Comics, Merchandise, Subscriptions, Shop Locator, and DC Power Visa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,9 +8,40 @@ export default function Main() {
                     </span>
                 </div>
             </div>
-            <div className="main-bottom">
-                <div className="container">
-
+            <div className="main-bottom text-white text-uppercase">
+                <div className="container h-100">
+                    <div className="row h-100 align-items-center justify-content-around px-3">
+                        <div className="col h-25 text-align-center">
+                            <img className="h-100 me-2" src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+                            <span>
+                                Digital Comics
+                            </span>
+                        </div>
+                        <div className="col h-25 text-align-center">
+                            <img className="h-100 me-2" src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
+                            <span>
+                                DC Merchandise
+                            </span>
+                        </div>
+                        <div className="col h-25 text-align-center">
+                            <img className="h-100 me-2" src="/img/buy-comics-subscriptions.png" alt="Subscription" />
+                            <span>
+                                Subscription
+                            </span>
+                        </div>
+                        <div className="col h-25 text-align-center">
+                            <img className="h-100 me-2" src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
+                            <span>
+                                Comic Shop Locator
+                            </span>
+                        </div>
+                        <div className="col h-25 text-align-center">
+                            <img className="h-100 me-2" src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
+                            <span>
+                                DC Power Visa
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
