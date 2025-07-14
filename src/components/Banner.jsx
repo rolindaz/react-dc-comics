c40ed527abd94153580fc7635bf226f9fb470902
rolindaz <rolinda.zamboni@gmail.com>
2025-07-14 17:52:22 +0200

    Update banner component with JS logic (bugged)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,38 +1,66 @@
+import digitalIcon from '/img/buy-comics-digital-comics.png'
+import merchandiseIcon from "/img/buy-comics-merchandise.png"
+import subscriptionIcon from "/img/buy-comics-subscriptions.png"
+import shopLocatorIcon from "/img/buy-comics-shop-locator.png"
+import dcVisaIcon from "/img/buy-dc-power-visa.svg"
+
 export default function Banner() {
+
+    let rowHTML = ''
+
+    const iconCols = [
+        {
+            image: digitalIcon,
+            title: "Digital Comics"
+        },
+        {
+            image: merchandiseIcon,
+            title: "DC Merchandise"
+        },
+        {
+            image: subscriptionIcon,
+            title: "Subscription"
+        },
+        {
+            image: shopLocatorIcon,
+            title: "Comic Shop Locator"
+        },
+        {
+            image: dcVisaIcon,
+            title: "DC Power Visa"
+        }
+    ]
+
+    console.log(iconCols);
+
+    iconCols.forEach((col) => {
+
+        const {
+            image,
+            title
+        } = col;
+
+
+        const colMarkup =
+            `
+            <div className="col text-align-center">
+                <img className="h-100 me-2" src=${image} alt=${title} />
+                <span className="align-middle">
+                    <${title} />
+                </span>
+            </div>
+            `
+
+        rowHTML += colMarkup
+
+    })
+    console.log(rowHTML);
+
     return (
         <div className="banner text-white text-uppercase">
             <div className="container h-100">
                 <div className="row h-100 align-items-center justify-content-around px-3">
-                    <div className="col text-align-center">
-                        <img className="h-100 me-2" src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
-                        <span className="align-middle">
-                            Digital Comics
-                        </span>
-                    </div>
-                    <div className="col text-align-center">
-                        <img className="h-100 me-2" src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
-                        <span className="align-middle">
-                            DC Merchandise
-                        </span>
-                    </div>
-                    <div className="col text-align-center">
-                        <img className="h-100 me-2" src="/img/buy-comics-subscriptions.png" alt="Subscription" />
-                        <span className="align-middle">
-                            Subscription
-                        </span>
-                    </div>
-                    <div className="col text-align-center">
-                        <img className="h-100 me-2" src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
-                        <span className="align-middle">
-                            Comic Shop Locator
-                        </span>
-                    </div>
-                    <div className="col text-align-center">
-                        <img className="h-100 me-2" src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
-                        <span className="align-middle">
-                            DC Power Visa
-                        </span>
-                    </div>
+                    {rowHTML}
                 </div>
             </div>
         </div>
