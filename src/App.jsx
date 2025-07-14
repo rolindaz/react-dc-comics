a80a75a6d406cac5cd1c9ef4d713282d2dbd3401
rolindaz <rolinda.zamboni@gmail.com>
2025-07-14 13:54:30 +0200

    Add Main and Footer components, integrate Google Fonts, and enhance Header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,24 @@ import Logo from './Logo'
 
 export default function Header() {
     return (
-        <header>
+        <header className='bg-white text-black text-uppercase'>
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="logo">
                     <Logo />
                 </div>
                 <nav>
-                    <ul className='d-flex gap-4'>
+                    <ul className='d-flex gap-4 list-unstyled'>
                         <li>
-                            <a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Collectibles</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">Fans</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Shop</a></li>
+                            <a className='text-black fw-bold text-decoration-none' href="#">Characters</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Comics</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Movies</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">TV</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Games</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Collectibles</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Videos</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Fans</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">News</a></li>
+                        <li><a className='text-black fw-bold text-decoration-none' href="#">Shop</a></li>
                     </ul>
                 </nav>
             </div>
